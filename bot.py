32b4efc4f5235ff5613f0f1e0d0874421bee8dd4
?????????????? ???????????µ???µ?????????? <?????????????? ????????????????????????>
2017-02-26 18:17:42 +0300

    Try use flask on Heroku

--- bot.py
+++ bot.py
@@ -1,18 +1,30 @@
 import telebot
-import config
 import os
-
-PORT = int(os.environ.get('PORT', '5000'))
-
-WEBHOOK_HOST = "https://hometorrbot.herokuapp.com/"
-WEBHOOK_PORT = PORT  # 443, 80, 88 или 8443 (порт должен быть открыт!)
-WEBHOOK_LISTEN = '0.0.0.0'  # На некоторых серверах придется указывать такой же IP, что и выше
-
-WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
-WEBHOOK_URL_PATH = "/%s/" % (config.token)
+import config
+from flask import Flask, request
 
 bot = telebot.TeleBot(config.token)
+
+server = Flask(__name__)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
     bot.reply_to(message, message.text)
+
+@server.route("/bot", methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url="https://hometorrbot.herokuapp.com/bot")
+    return "!", 200
+
+server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
+server = Flask(__name__)
