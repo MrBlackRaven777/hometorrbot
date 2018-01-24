712721215ffe1aa3a51bf70bf45301da5fa5ba18
BlackRaven <cakeman3000@gmail.com>
2018-01-24 18:02:55 +0300

    Moved to pickle
    moved to pickle instead shelve, cause it works as expected. Done explorer, works almost well.

--- bot.py
+++ bot.py
@@ -1,34 +1,122 @@
-import telebot
-import os
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import config
-import sys
-from flask import Flask, request
+import utils
+import os
+import logging
 
-bot = telebot.TeleBot(config.token)
-print 'Im here!'
-sys.stdout.flush()
 
-server = Flask(__name__)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+upd = Updater(token=config.token)
+dsp = upd.dispatcher
 
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    bot.reply_to(message, message.text)
 
-@server.route("/bot", methods=['POST'])
-def getMessage():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
 
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url="https://hometorrbot.herokuapp.com/")
-    return "!", 200
+def path(bot, upd):
+    utils.pickle_write(upd.message.chat_id, "is_expl_on", False)
+    fav_list = list(utils.pickle_read(upd.message.chat_id,'favorites').keys())
+    markup = telegram.ReplyKeyboardMarkup(
+        utils.create_markup(fav_list, upd.message.chat_id), one_time_keyboard=True)
+    bot.sendMessage(chat_id=upd.message.chat_id,
+                    text="Choose folder from favorites, file system or cancel", reply_markup=markup, one_time_keyboard=True)
 
-server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
-server = Flask(__name__)
-print 'Im here now!'
+  
+def echo(bot, upd):
+    msg = upd.message.text
+    ch_id = upd.message.chat_id
+    favorites = utils.pickle_read(ch_id,'favorites')
+    fav_list = list(favorites.keys())
+    full_path = utils.pickle_read(ch_id, 'curr_dir')
+#    print(full_path)
+    dirs = next(os.walk(full_path))[1]
+    if utils.pickle_read(ch_id,'is_expl_on') == True:
+        if msg in dirs:
+            print(2)
+            utils.pickle_write(ch_id, "curr_dir", full_path + "\\" + msg)
+            answer = utils.explorer(ch_id, msg)
+            markup = telegram.ReplyKeyboardMarkup(
+                    utils.create_markup(answer, ch_id))
+            answer = None
+        elif msg == "Done":
+            answer = "Choosen folder: " +  utils.pickle_read(ch_id,'curr_dir')
+            markup = telegram.ReplyKeyboardRemove()
+        else:
+            print(3)
+            answer = "No directory named \"{0}\" in \"{1}\". Try again".format(msg, full_path)
+            markup = telegram.ReplyKeyboardMarkup(utils.create_markup(dirs, ch_id))
+        
+        bot.sendMessage(chat_id=ch_id,
+                        text=answer, reply_markup=markup)
+    else:
+        if msg == 'Choose folder':
+            print(4)
+            utils.pickle_write(ch_id, "is_expl_on", True)
+            full_path = str(utils.pickle_read(ch_id,'favorites').get("Root"))
+            print(full_path)
+            utils.pickle_write(ch_id, "curr_dir", full_path)
+            answer = utils.explorer(ch_id, "")
+            markup = telegram.ReplyKeyboardMarkup(
+                        utils.create_markup(answer, ch_id))
+            answer = "Choose destination folder"
+            
+        elif msg in fav_list:
+            print(1)
+            utils.pickle_write(ch_id, "curr_dir", favorites.get(msg))
+            answer = "Choosen folder: " + favorites.get(msg)
+            markup = telegram.ReplyKeyboardMarkup(
+                    utils.create_markup(fav_list, ch_id))
+        else:
+            answer = "Unknown command"
+            markup = telegram.ReplyKeyboardRemove()
+        bot.sendMessage(chat_id=ch_id,
+                        text=answer, reply_markup=markup, one_time_keyboard=True)
+
+
+def start(bot, upd):
+#Starts bot, creates shelves for new user and setting up them with default folders for enviroment, where bot working
+    new_user = True
+    for d, dirs, files in os.walk(os.getcwd()):
+        for f in files:   
+            if str(upd.message.chat_id) in f:
+                    new_user = False
+    if new_user == True:
+        favorites = utils.os_choose()
+        curr_dir = favorites.pop("curr_dir")
+        utils.pickle_write(upd.message.chat_id, "curr_dir", curr_dir)
+        utils.pickle_write(upd.message.chat_id, "favorites", favorites)
+        utils.pickle_write(upd.message.chat_id, "is_expl_on", False)
+        reply_text = "Yours OS is: " + os.name + ". Your storage successfully created. Welcome!" 
+    else:
+        reply_text = "You're already my user. Send me /reset to destroy your personal settings"
+    bot.sendMessage(chat_id=upd.message.chat_id, text=reply_text)
+
+
+def reset(bot, upd):
+    markup = telegram.ReplyKeyboardRemove()
+    bot.sendMessage(chat_id=upd.message.chat_id, text=utils.shelve_remove(upd.message.chat_id), reply_markup=markup)    
+    
+
+def my_id(bot, upd):
+    bot.sendMessage(chat_id=upd.message.chat_id, text= "Your id is: " + str(upd.message.chat_id))
+    
+
+
+path_handler = CommandHandler("path", path)
+dsp.add_handler(path_handler)
+
+start_handler = CommandHandler("start", start)
+dsp.add_handler(start_handler)
+
+reset_handler = CommandHandler("reset", reset)
+dsp.add_handler(reset_handler)
+
+echo_handler = MessageHandler(Filters.text, echo)
+dsp.add_handler(echo_handler)
+
+myid_handler = CommandHandler("myid", my_id)
+dsp.add_handler(myid_handler)
+
+upd.start_polling()
+upd.idle()
---
+++ utils.py
@@ -0,0 +1,102 @@
+import os
+import shelve
+import sys
+import pickle
+#from config import favorites as fav
+
+
+def create_markup(button_list, sh_id, num=1):
+    n_rows = [0, 0, 0]
+    blen = len(button_list)
+    keyboard = []
+    if blen == 0:
+        pass
+    elif blen < 10:
+        for i in range(0,blen):
+            n_rows[i % 3] = n_rows[i % 3] + 1
+        n = 0
+        keyboard = [[0] * n_rows[0], [0] * n_rows[1], [0] * n_rows[2]]
+        for l in range(3):
+            j = n_rows[l]
+            for k in range(0, j):
+                keyboard[l][k] = button_list[n]
+                n += 1
+    else:
+        create_markup(button_list[:8], sh_id)
+#        TODO - correctly creating markup for > 9 folders
+#        start = (num-1)*9
+#        stop = blen if blen <= num*9 else num*9
+#        for i in range(0, stop%9-1):
+#            n_rows[i % 3] = n_rows[i % 3] + 1
+#        n = start
+#        keyboard = [[0] * n_rows[0], [0] * n_rows[1], [0] * n_rows[2]]
+#        for l in range(3):
+#            j = n_rows[l]
+#            for k in range(0, j):
+#                keyboard[l][k] = button_list[n]
+#                n += 1
+    if pickle_read(sh_id, "is_expl_on") == True:
+        keyboard.append(["Back", "Done", "More"])
+    else:
+        keyboard.append(['Choose folder', 'Cancel'])
+    return keyboard
+
+
+def explorer(id, next_dir):
+    curr_dir = pickle_read(id, "curr_dir")
+    if next_dir != "":
+        next_dir = "\\" + next_dir
+    if curr_dir is None:
+        return "No path"
+    else:
+        
+        full_path = curr_dir + next_dir
+        print(full_path)
+        try:
+            dirs_list = next(os.walk(full_path))[1]
+            print(dirs_list)
+            return dirs_list
+        except:
+            print(sys.exc_info())
+            dirs_list = next(os.walk(curr_dir))[1]
+            return dirs_list
+
+
+def pickle_write(id, param_name, state):
+    root_dir = os.getcwd()
+    os.chdir("users_storage")
+    name = 'pickle_{0}_{1}.txt'.format(id, param_name)
+    print("Going to write in {3}\\{0} value: {1} : {2}".format(name, param_name, state, os.getcwd()))
+    try:    
+        storage = open(name, 'wb')
+        pickle.dump(state, storage)
+        storage.close()
+    except:
+        print(sys.exc_info())
+    os.chdir(root_dir)
+
+
+def pickle_read(id, param_name):
+    root_dir = os.getcwd()
+    os.chdir("users_storage")
+    name = 'pickle_{0}_{1}.txt'.format(id, param_name)
+    try:    
+        storage = open(name, 'rb')
+        data = pickle.load(storage)
+        storage.close()
+        
+    except:
+        print(sys.exc_info())
+    os.chdir(root_dir)
+    return data
+
+    
+def os_choose():  
+    if os.name == "nt":
+        return {"curr_dir": os.path.expanduser("~"), "Root": "C:/", "Download": os.path.expanduser("~\\Downloads"), "Video": os.path.expanduser("~\\Videos"), "Default folder": os.getcwd()}
+    elif os.name == "posix":
+        return {"curr_dir": os.path.expanduser("~"), "Root": "/", "Download": os.path.expanduser("~/Download"), "Movies": os.path.expanduser("~/Movies"), "Default folder": os.getcwd()}
+    else:
+        return {"curr_dir": "/"}
+    
+#pickle_write(332761, 'Root', 'C:\\')
