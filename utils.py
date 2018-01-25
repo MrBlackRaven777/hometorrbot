44637b3f9bdedc444836b03e14378692ab358cf9
BlackRaven <cakeman3000@gmail.com>
2018-01-25 18:10:45 +0300

    keyboard update
    almost done in one string, see test.py

--- utils.py
+++ utils.py
@@ -23,25 +23,35 @@ def create_markup(button_list, sh_id, num=1):
                 keyboard[l][k] = button_list[n]
                 n += 1
     else:
-        create_markup(button_list[:8], sh_id)
-#        TODO - correctly creating markup for > 9 folders
-#        start = (num-1)*9
-#        stop = blen if blen <= num*9 else num*9
-#        for i in range(0, stop%9-1):
-#            n_rows[i % 3] = n_rows[i % 3] + 1
-#        n = start
-#        keyboard = [[0] * n_rows[0], [0] * n_rows[1], [0] * n_rows[2]]
-#        for l in range(3):
-#            j = n_rows[l]
-#            for k in range(0, j):
-#                keyboard[l][k] = button_list[n]
-#                n += 1
+#        create_markup(button_list[:8], sh_id)
+##        TODO - correctly creating markup for > 9 folders
+        start = (num-1)*9
+        stop = blen if blen <= num*9 else num*9
+        for i in range(0, stop%9-1):
+            n_rows[i % 3] = n_rows[i % 3] + 1
+        n = start
+        keyboard = [[0] * n_rows[0], [0] * n_rows[1], [0] * n_rows[2]]
+        for l in range(3):
+            j = n_rows[l]
+            for k in range(0, j):
+                keyboard[l][k] = button_list[n]
+                n += 1
     if pickle_read(sh_id, "is_expl_on") == True:
         keyboard.append(["Back", "Done", "More"])
     else:
         keyboard.append(['Choose folder', 'Cancel'])
     return keyboard
 
+
+def create_markup_new(button_list, id, pg_num=1):
+    n_rows = [0, 0, 0]
+    blen = len(button_list)
+    keyboard = []
+    boobs = 9 #not my fetish, just max number of Buttons On One Board Setting =) 
+    keyboard = [[button_list[x+y] for x in range(2)] for y in range(2)]
+    print(keyboard)
+#    for i in range(boobs):
+#        keyboard[k]=keyboard[k].append(button_list[i])
 
 def explorer(id, next_dir):
     curr_dir = pickle_read(id, "curr_dir")
